20368e9d008967bf0dc567f95aa543fcc51b07c2
Symeon Zapsis <s.zapsis@gmail.com>
2026-06-24 21:43:43 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes_of_Might_and_Magic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
