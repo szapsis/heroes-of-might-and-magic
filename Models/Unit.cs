30b113c688f3312fe36d93e9bc94e80c87b7ff6d
Symeon Zapsis <s.zapsis@gmail.com>
2026-06-24 23:41:46 +0300

    Implement unit model and battle system

---
+++ Models/Unit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes_of_Might_and_Magic.Models
+{
+    public class Unit
+    {
+        public string Name { get; private set; }
+        public int Count { get; private set; }
+        public int Attack { get; private set; }
+        public int Defense { get; private set; }
+        public int MinDamage { get; private set; }
+        public int MaxDamage { get; private set; }
+
+        public int Health { get; private set; }
+
+        public int TotalHealth { get; private set; }
+
+        public Unit(string name, int count, int attack, int defense, int minDamage, int maxDamage, int health)
+        {
+            Name = name;
+            Count = count;
+            Attack = attack;
+            Defense = defense;
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+            Health = health;
+            TotalHealth = Health * Count;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            TotalHealth -= damage;
+
+            if (TotalHealth < 0)
+            {
+                TotalHealth = 0;
+            }
+        }
+
+        public bool IsAlive
+        {
+            get { return TotalHealth > 0; }
+        }
+
+        private static Random random = new Random();
+
+        public int GetDamage()
+        {
+            return random.Next(MinDamage, MaxDamage + 1);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Heroes_of_Might_and_Magic.Models;
 
 namespace Heroes_of_Might_and_Magic
 {
@@ -10,6 +11,58 @@ namespace Heroes_of_Might_and_Magic
     {
         static void Main(string[] args)
         {
+
+            Unit pikeman = new Unit(name: "Pikeman", count: 10, attack: 4, defense: 5, minDamage: 1, maxDamage: 3, health: 10);
+            Unit skeleton = new Unit(name: "Skeleton", count: 15, attack: 5, defense: 4, minDamage: 1, maxDamage: 3, health: 6);
+
+            //Console.WriteLine($"{pikeman.Name} | ATK: {pikeman.Attack} | DEF: {pikeman.Defense} | DMG: {pikeman.Damage} | HP: {pikeman.Health}");
+            //Console.WriteLine($"{skeleton.Name} | ATK: {skeleton.Attack} | DEF: {skeleton.Defense} | DMG: {skeleton.Damage} | HP: {skeleton.Health}");
+
+
+            Fight(pikeman, skeleton);
+
+        }
+
+        static void Fight(Unit unit1, Unit unit2)
+        {
+            int round = 1;
+
+            while (unit1.IsAlive && unit2.IsAlive)
+            {
+                Console.WriteLine("=== Battle Start ===");
+                Console.WriteLine($"{unit1.Count} {unit1.Name} (HP: {unit1.TotalHealth})");
+                Console.WriteLine("VS");
+                Console.WriteLine($"{unit2.Count} {unit2.Name} (HP: {unit2.TotalHealth})");
+                Console.WriteLine();
+
+                Console.WriteLine($"=== Round {round} ===");
+
+                int damage = unit1.GetDamage();
+                Console.WriteLine($"{unit1.Name} attacks {unit2.Name} for {damage} damage");
+                unit2.TakeDamage(damage);
+                Console.WriteLine($"{unit2.Count} {unit2.Name} HP: {unit2.TotalHealth}");
+
+                if (!unit2.IsAlive)
+                {
+                    Console.WriteLine($"{unit2.Count} {unit2.Name} died. {unit1.Name} wins!");
+                    break;
+                }
+
+                damage = unit2.GetDamage();
+                Console.WriteLine($"{unit2.Name} attacks {unit1.Name} for {damage} damage");
+                unit1.TakeDamage(damage);
+                Console.WriteLine($"{unit1.Count} {unit1.Name} HP: {unit1.TotalHealth}");
+
+                if (!unit1.IsAlive)
+                {
+                    Console.WriteLine($"{unit1.Count} {unit1.Name} died. {unit2.Name} wins!");
+                    break;
+                }
+
+                Console.WriteLine();
+                round++;
+            }
+
         }
     }
 }
